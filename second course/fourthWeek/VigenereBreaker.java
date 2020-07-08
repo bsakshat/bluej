cb53e92b2450fccac522fe921e1fe8a189819a85
Sakshat Bhattarai <bhattaraisakshat07@gmail.com>
2020-07-08 17:31:01 -0400

    week-4 start

---
+++ second course/fourthWeek/VigenereBreaker.java
@@ -0,0 +1,22 @@
+package fourthWeek;
+
+import java.util.*;
+import edu.duke.*;
+
+public class VigenereBreaker {
+    public String sliceString(String message, int whichSlice, int totalSlices) {
+        
+        return "WRITE ME!";
+    }
+
+    public int[] tryKeyLength(String encrypted, int klength, char mostCommon) {
+        int[] key = new int[klength];
+        
+        return key;
+    }
+
+    public void breakVigenere () {
+        
+    }
+    
+}
