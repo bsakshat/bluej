df7810b3464f17278e6c43ae6147d4e5f61fb1fb
Sakshat Bhattarai <bhattaraisakshat07@gmail.com>
2020-07-09 11:52:59 -0400

    first

--- second course/fourthWeek/VigenereBreaker.java
+++ second course/fourthWeek/VigenereBreaker.java
@@ -3,20 +3,34 @@ package fourthWeek;
 import java.util.*;
 import edu.duke.*;
 
-public class VigenereBreaker {
-    public String sliceString(String message, int whichSlice, int totalSlices) {
-        
-        return "WRITE ME!";
+public class VigenereBreaker {    
+    private String sliceString(String message, int whichSlice, int totalSlices) {
+        String slice = "";
+        for (int i = whichSlice; i < message.length(); i += totalSlices) {
+            slice += message.charAt(i);
+        }
+        return slice;
     }
 
     public int[] tryKeyLength(String encrypted, int klength, char mostCommon) {
         int[] key = new int[klength];
-        
+        CaesarCracker cc = new CaesarCracker(mostCommon);
+        for (int i = 0; i < klength; i++) {
+            String slice = sliceString(encrypted, i, klength);
+            int singleKey = cc.getKey(slice);
+            key[i] = singleKey;
+            System.out.println(singleKey);
+        }
         return key;
     }
 
     public void breakVigenere () {
-        
+        FileResource fr = new FileResource();
+        String message = fr.asString();
+        int[] key = tryKeyLength(message, 4, 'e');
+        VigenereCipher vg = new VigenereCipher(key);
+        String answer = vg.decrypt(message);
+        System.out.println(answer);
     }
     
 }
